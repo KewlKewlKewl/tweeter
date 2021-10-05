5298bbfdb979aadd1d9fbb6438886fa1bde95e72
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-05 21:30:44 +0000

    initial commit plus nav bar, header and main container CSS

--- server/index.js
+++ server/index.js
@@ -13,24 +13,24 @@ app.use(express.static("public"));
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
-// The `data-helpers` module provides an interface to the database of tweets.
+// The `data-helpers` module provides an interface to the database of tweets. //REALLY IMPORTANT/COOL TRICK
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
 // (hint hint).
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const DataHelpers = require("./lib/data-helpers.js")(db); //pases the db object in here to the helper funcitons imported/required from the datahelpers page
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object //IMPORTANT SAME THING AS THE DB DATA HELPERS PROCESS ABOVE
 // so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+const tweetsRoutes = require("./routes/tweets")(DataHelpers); //PASSES THE DATA HELPERS THAT INTERACT WITH OUR DB TO OUR ROUTES PAGE.
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+app.use("/tweets", tweetsRoutes); //this intializes the main tweets page with all its routes and code created by passing in the datahlpers to routes tweet above.
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,14 +1,14 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+const simulateDelay = require("./util/simulate-delay"); // JUST ADDS A NICE AFFECT/UI TO OUR SPA
 
 // Defines helper functions for saving and getting tweets, using the database `db`
-module.exports = function makeDataHelpers(db) {
+module.exports = function makeDataHelpers(db) { //this DB passed in on the rquired will be used in each function object below. COOL TRICK
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
+    saveTweet: function(newTweet, callback) { //asycn function 
       simulateDelay(() => {
         db.tweets.push(newTweet);
         callback(null, true);
@@ -16,10 +16,10 @@ module.exports = function makeDataHelpers(db) {
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
+    getTweets: function(callback) { //aysnc function
       simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        const sortNewestFirst = (a, b) => a.created_at - b.created_at; //object deconstruction function!!! THIS IS A ARROW NOTATION FUCNTION. uses created.at property of tweet objects. its essntially the callback function created to pass into .sort method. SEE DOCUMENTATION ON .SORT
+        callback(null, db.tweets.sort(sortNewestFirst)); //calling .sort function on array using the callback function pased in created above. the callback is used to give the .sort sorting rules. SEE MDN
       });
     }
 
